c4674cd8e7d8186e0dfa1282b417849631731cdd
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-05-02 20:58:57 +0300

    Добавление бесконечного цикла с перемешкой списка с названием файлов, для выкладывания медиа в тг,

--- python-telegram-bot.py
+++ python-telegram-bot.py
@@ -1,14 +1,29 @@
 import os
 import time
-
+import random
 import telegram.ext
 from dotenv import load_dotenv
 
-load_dotenv()
-TOKEN = os.environ['api_token_tg']
-bot = telegram.Bot(token=TOKEN)
 
-chat_id = os.environ['chat_id_cosmos']
-bot.send_message(chat_id=chat_id,
-                 text="Привет, в этой группе ты найдешь много замечательных фотографий связанных с космосом!")
-bot.send_document(chat_id=chat_id, document=open('img/apod.jpg', 'rb'))
+def main():
+    load_dotenv()
+    token = os.environ['api_token_tg']
+    bot = telegram.Bot(token=token)
+
+    chat_id = os.environ['chat_id_cosmos']
+    time_delay = os.environ['time_delay']
+
+    images = []
+    for root, dirs, files in os.walk("img"):
+        for filename in files:
+            images.append(filename)
+
+    while True:
+        for filename in images:
+            bot.send_document(chat_id=chat_id, document=open(f'img/{filename}', 'rb'))
+            time.sleep(int(time_delay))
+        random.shuffle(images)
+
+
+if __name__ == '__main__':
+    main()
